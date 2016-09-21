d97fd01f8bcf0cbe48e4c682ca4fb030fedbe8e1
vsai911 <varunsai91@gmail.com>
2016-09-21 05:41:26 +0000

    Run server on 3000 and 3001

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,9 +70,9 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 3000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
@@ -96,7 +96,7 @@ module.exports = function (grunt) {
       },
       test: {
         options: {
-          port: 9001,
+          port: 3001,
           middleware: function (connect) {
             return [
               connect.static('.tmp'),
